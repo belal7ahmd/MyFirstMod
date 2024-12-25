ae893f1723766574921dcced3600cf8a429c5574
Belal Ahmed <belal.ahmd219@gmail.com>
2024-12-25 06:56:54 +0200

    Initial commit

---
+++ src/main/java/net/belal/firstmod/MyFirstMod.java
@@ -0,0 +1,18 @@
+package net.belal.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyFirstMod implements ModInitializer {
+	public static final String MOD_ID = "firstmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/belal/firstmod/MyFirstModClient.java
@@ -0,0 +1,10 @@
+package net.belal.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
